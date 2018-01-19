32b83e24997df36461c5b78ef658e64143f33514
Jianyu <bb1034084869@163.com>
2018-01-19 22:26:46 +0800

    使用Flask-SQLAlchemy改写todolist
    使用Flask-SQLAlchemy改写todolist

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #-*- coding:UTF-8 -*-
 
 from __future__ import unicode_literals
-import time
-import pymysql
-from flask import (Flask,render_template,g,session,redirect,url_for,
+
+from flask import (Flask,render_template,session,redirect,url_for,
                    request,flash,abort)
 from forms import TodoListForm
 from flask_bootstrap import Bootstrap#初始化Flask-bootstrap，把程序实例传入构造方法进行初始化
-
+from ext import db
+from models import TodoList
 
 
 SECRET_KEY = 'this is key'#Session, Cookies以及一些第三方扩展都会用到SECRET_KEY值，
@@ -19,30 +19,11 @@ bootstrap = Bootstrap(app)
 
 app.config['USERNAME']='admin'
 app.config['PASSWORD']='admin'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123456@localhost/test"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+db.init_app(app)
 
-
-
-
-def connect_db():
-    """returns a new connection to the database."""
-    return pymysql.connect(host='127.0.0.1',
-                           user='root',
-                           passwd='123456',
-                           db='test',
-                           charset='utf8')
-    
-@app.before_request
-def before_request():
-    """make sure we are connected to the database each request."""
-    g.db = connect_db()
-    
-
-@app.after_request
-def after_request(response):
-    """close the database again at the end of the request."""
-    g.db.close()
-    return response
 
 @app.route('/', methods=['GET', 'POST'])
 def show_todo_list():
@@ -50,38 +31,28 @@ def show_todo_list():
         return redirect(url_for('login'))#如果存储的session中logged_in值为False则重定向到login（ url）
     form = TodoListForm()
     if request.method =='GET':
-        sql='select id,user_id,title,status,create_time from todolist'
-        with g.db.cursor() as cur:
-            cur.execute(sql)
-            todo_list = [dict(id=row[0],user_id=row[1],title=row[2],status=bool(row[3]),create_time=row[4]) 
-                         for row in cur.fetchall()]
-            g.db.commit()
-        return render_template('index.html',todo_list=todo_list,form=form)
+        todolists = TodoList.query.all()
+        return render_template('index.html',todolists=todolists,form=form)
     else:
         if form.validate_on_submit():
-            title = request.form['title']
-            status = request.form['status']
-            with g.db.cursor() as cur:
-                sql = """insert into todolist(`user_id`, `title`, `status`,
-                `create_time`) values ({0}, '{1}', {2}, {3})""".format( 1, title, status, int(time.time()))
-                app.logger.info(sql)
-                cur.execute(sql)
-                g.db.commit()
+            todolist = TodoList(1,form.title.data,form.status.data)
+            db.session.add(todolist)
+            db.session.commit()
             flash('You have add a new todo list')
+        
         else:
             flash(form.errors)
         return redirect(url_for('show_todo_list'))
-
+            
 @app.route('/delete')
 def delete_todo_list():
     id = request.args.get('id',None)
     if id is None:
         abort(404)
     else:
-        sql = "delete from todolist where id = {0}".format(id)
-        with g.db.cursor() as cur:
-            cur.execute(sql)
-            g.db.commit()
+        todolist = TodoList.query.filter_by(id=id).first_or_404()
+        db.session.delete(todolist)
+        db.session.commit()
         flash('You have delete a todo list')
         return redirect(url_for('show_todo_list'))
 
@@ -112,9 +83,6 @@ def logout():
     #显示Flash的信息需要在模板中使用
     #get_flashed_messages()函数获取Flash的信息，并渲染信息。
     return redirect(url_for('login'))
-
-
-
 
 
 
